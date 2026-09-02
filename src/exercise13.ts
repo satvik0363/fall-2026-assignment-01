2db2ce54ea8c8494254abc1104b6a1751cf696c4
satvik0363 <satvik0803@gmail.com>
2026-09-02 18:05:09 -0400

    13

--- src/exercise13.ts
+++ src/exercise13.ts
@@ -1,5 +1,16 @@
-export type TodoItem = {};
+export type TodoItem = {userId: number; id: number; title: string; completed: boolean};
 
 export async function fetchTodoSafe(todoId: number): Promise<TodoItem | null> {
-  return null;
+  try {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`);
+
+    if (!response.ok) {
+      return null;
+    }
+    const data: TodoItem = (await response.json()) as TodoItem;
+    return data;
+  }
+  catch {
+    return null;
+  }
 }
