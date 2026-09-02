2855b6df1d32628bb5dfca350782a8862311e199
satvik0363 <satvik0803@gmail.com>
2026-09-02 14:38:40 -0400

    Exercise 04

--- src/exercise04.ts
+++ src/exercise04.ts
@@ -1,11 +1,20 @@
-export type Circle = {};
+export type Circle = {kind: "circle"; radius: number};
 
-export type Rectangle = {};
+export type Rectangle = {kind: "rectangle"; width: number; height: number};
 
-export type Square = {};
+export type Square = {kind: "square"; sideLength: number};
 
 export type Shape = Circle | Rectangle | Square;
 
 export function calculateArea(shape: Shape): number {
+  if (shape.kind == "circle") {
+    return Math.PI * (shape.radius ** 2);
+  }
+  if (shape.kind == "square") {
+    return shape.sideLength ** 2;
+  }
+  if (shape.kind == "rectangle") {
+    return shape.width * shape.height;
+  }
   return 0;
 }
