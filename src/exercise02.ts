ba4ed3de9a435feb972271e960ac4c86cff0e991
satvik0363 <satvik0803@gmail.com>
2026-09-02 13:32:23 -0400

    Test

--- src/exercise02.ts
+++ src/exercise02.ts
@@ -1,3 +1,13 @@
 export function transcribeDNA(dna: string): string {
-  return '';
+  let output = '';
+  for (let i: number = 0; i < dna.length; i++) {
+    if (dna[i] == 'A') output += 'U';
+    else if (dna[i] == 'T') output += 'A';
+    else if (dna[i] == 'C') output += 'G';
+    else if (dna[i] == 'G') output += 'C';
+    else {
+      throw new Error("Not a valid DNA letter");
+    }
+  }
+  return output;
 }
--- src/exercise03.ts
+++ src/exercise03.ts
@@ -1,5 +1,8 @@
 export function getInventoryValue(
   inventory: Array<[string, number, number]>,
 ): number {
+  let minimum = 5;
+  const greater = inventory.filter(quantity => minimum);
+  const sum = inventory.reduce((quantity, price) => quantity * price);
   return 0;
 }
