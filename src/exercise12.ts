4352bb704bae67704eaa5ca86483433464961853
satvik0363 <satvik0803@gmail.com>
2026-09-02 17:50:06 -0400

    12

--- src/exercise12.ts
+++ src/exercise12.ts
@@ -1,3 +1,7 @@
+export type RemoteUser = {id: number; name: string; email: string}
+
 export async function fetchUserEmails(): Promise<string[]> {
-  return [];
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: RemoteUser[] = (await response.json()) as RemoteUser[];
+  return users.map((user) => user.email);
 }
