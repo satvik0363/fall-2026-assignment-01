7d9a7fa83b3cd9bd538a31d647319cf026f0df19
satvik0363 <satvik0803@gmail.com>
2026-09-02 18:56:18 -0400

    15

--- src/exercise15.ts
+++ src/exercise15.ts
@@ -1,6 +1,15 @@
+import fs from 'fs/promises';
+
+export type CommentSummary = {postId: number; id: number; commenterEmail: string};
+
 export async function processCommentsPipeline(
   targetPostId: number,
   outputPath: string,
 ): Promise<number> {
-  return 0;
+  const data = await fetch(`https://jsonplaceholder.typicode.com/posts/${targetPostId}/comments`);
+  const raw: Array<{postId: number; id: number; email: string}> = (await data.json()) as Array<{ postId: number; id: number; email: string }>;
+  const summaries: CommentSummary[] = raw.map((comment) => ({postId: comment.postId, id: comment.id, commenterEmail: comment.email}));
+  const filter = summaries.filter((comment) => !comment.commenterEmail.toLowerCase().endsWith(".org"));
+  await fs.writeFile(outputPath, JSON.stringify(filter));
+  return filter.length;
 }
